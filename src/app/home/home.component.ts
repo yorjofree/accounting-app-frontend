7723cdbc035d977e46847c573792b4dc04a464ad
Francesco Iorio <ioriofre_99@hotmail.it>
2024-11-28 15:50:10 +0100

    basic routing with /home and /actionlist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {ActionListComponent} from '../action-list/action-list.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ActionListComponent],
+  imports: [ActionListComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
