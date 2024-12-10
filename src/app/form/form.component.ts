efc494d17ea2d8ff6f8a6c29da2dc60288868b48
Francesco Iorio <ioriofre_99@hotmail.it>
2024-12-10 18:37:35 +0100

    used validators for form control

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, viewChild } from '@angular/core';
+import { Component, computed, inject, signal, viewChild } from '@angular/core';
 import { TypeFieldComponent } from './type-field/type-field.component';
 import { AccountFieldComponent } from './account-field/account-field.component';
 import { AmountFieldComponent } from './amount-field/amount-field.component';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -16,9 +16,27 @@ export class FormComponent {
   typeTest = 10;
 
   formBuilder = inject(FormBuilder);
-  formGroup = this.formBuilder.group({
+  recordForm = this.formBuilder.group({
     type: ['entrata'],
     account: ['cassa'],
-    amount: [10.00]
+    amount: [10.00, [Validators.required, Validators.pattern('[0-9]*')]]
   });
+
+  newRecord = {
+    type: signal(''),
+    account: signal(''),
+    amount: signal('')
+  };
+
+  onSubmit() {
+    if (this.recordForm.value.type) {
+      this.newRecord.type.set(this.recordForm.value.type);
+    }
+    if (this.recordForm.value.account) {
+      this.newRecord.account.set(this.recordForm.value.account);
+    }
+    if (this.recordForm.value.amount) {
+      this.newRecord.amount.set(this.recordForm.value.amount.toString());
+    }
+  }
 }
