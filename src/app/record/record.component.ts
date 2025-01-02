09e459f31a72d3838d7ba82db37d60f579e836c6
Francesco Iorio <ioriofre_99@hotmail.it>
2025-01-02 13:19:59 +0100

    added optionMenu to records

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { Record } from '../record.interface';
 
 @Component({
@@ -9,4 +9,26 @@ import { Record } from '../record.interface';
 })
 export class RecordComponent {
   record = input.required<Record>();
+  name = computed(() => "optionMenu" + this.record().id);
+
+  showOptionMenu(e: MouseEvent): void {
+    
+    this.hideOptionMenus();
+
+    const optionMenu = document.getElementById("optionMenu" + this.record().id);
+    if (optionMenu !== null) {
+      optionMenu.style.display = "initial";
+      console.log("changed display property of element with id = " + optionMenu.id);
+      e.stopPropagation();
+    }
+  }
+
+  hideOptionMenus(): void {
+    const optionMenus = document.getElementsByClassName("option-menu") as HTMLCollectionOf<HTMLElement>;
+    for (const optionMenu of optionMenus) {
+      if (optionMenu.style.display == "initial") {
+        optionMenu.style.display = "none";  
+      }
+    }
+  }  
 }
